033896fc026cfe3684ade3cd661289e51979d9af
João Pedro Sodré Freitas <ucabpsf@ucl.ac.uk>
2023-06-29 18:18:11 +0100

    connection to db

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,33 @@
 import express from "express";
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { __prod__ } from "./constants";
+import { join } from "path";
+
+export const conn = new DataSource({
+  type: "postgres",
+  database: "vscribe",
+  username: "postgres",
+  password: "postgres",
+  entities: [join(__dirname, "./entities", "*.*")],
+  logging: !__prod__,
+  synchronize: !__prod__,
+  })
 
 const main = async () => {
+
+  try{
+    await conn.initialize();
+    console.log('Database connection established:', conn.isInitialized);
+  } catch (error) {
+    console.error('Database connection error:', error);
+  }
+  
   const app = express();
   app.get("/", (_req, res) => {
     res.send("hello");
   });
+
   app.listen(3002, () => {
     console.log("listening on localhost:3002");
   });
