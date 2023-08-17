6b70c1d349d859398114aa006fa5ce4485649274
JPSod <117471577+JPSod@users.noreply.github.com>
2023-08-17 20:51:59 +0000

    small change to trainer

--- BERT/BERT_trainer.py
+++ BERT/BERT_trainer.py
@@ -1,12 +1,12 @@
 # Example code to train and save the BERT model (using transformers library)
-from transformers import BertTokenizer, BertForSequenceClassification
+from transformers import AutTokenizer, BertForSequenceClassification
 
 # Load and preprocess your labeled dataset
-# ...
+
 
 # Initialize the BERT model and tokenizer
-model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=1)
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+model = BertForSequenceClassification.from_pretrained('microsoft/codebert-base', num_labels=1)
+tokenizer = AutoTokenizer.from_pretrained("microsoft/codebert-base")
 
 # Fine-tune the model on your dataset
 # ...
